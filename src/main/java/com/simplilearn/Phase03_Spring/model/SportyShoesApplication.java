53aa6be5f447b547cb381122c12d55ae11e5e08b
thakur-ranjeet <88493801+thakur-ranjeet@users.noreply.github.com>
2023-05-22 22:44:12 +0530

    Add files via upload
    Added all the files.

---
+++ src/main/java/com/simplilearn/Phase03_Spring/model/SportyShoesApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Phase03_Spring.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/Phase03_Spring/model/User.java
@@ -0,0 +1,25 @@
+package com.simplilearn.Phase03_Spring.model;
+
+public class User {
+public String name;
+public String password;
+public String searchName;
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getPassword() {
+return password;
+}
+public void setPassword(String password) {
+this.password = password;
+}
+public String getSearchName() {
+return searchName;
+}
+public void setSearchName(String searchName) {
+this.searchName = searchName;
+}
+}
